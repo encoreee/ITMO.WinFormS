36ba09311ee922a904a07a3e35a358d039542006
Александр Райков <alexraykov2000@gmail.com>
2020-12-04 16:49:45 +0300

    6_2

---
+++ ITMO.WinForms.WinGrafSinLine6_1/Operation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.WinForms.WinGrafSinLine6_1
+{
+    public class Operation
+    {
+        public static double SummSin(int x, double a, double b)
+        {
+            return Math.Sin(x * a) + Math.Sin(x * b);
+        }
+    }
+}
