50326c1aa1d65dd1794ea269c55d64c80a376944
Александр Райков <alexraykov2000@gmail.com>
2020-12-07 14:42:09 +0300

    7_4

---
+++ ITMO.WinForms.ControlLab7_4/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace ITMO.WinForms.WinAsynchMethod7_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMax = new System.Windows.Forms.Label();
+            this.MaxValue = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblMax
+            // 
+            this.lblMax.AutoSize = true;
+            this.lblMax.Location = new System.Drawing.Point(10, 11);
+            this.lblMax.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblMax.Name = "lblMax";
+            this.lblMax.Size = new System.Drawing.Size(52, 13);
+            this.lblMax.TabIndex = 0;
+            this.lblMax.Text = "MaxVaue";
+            // 
+            // MaxValue
+            // 
+            this.MaxValue.Location = new System.Drawing.Point(79, 8);
+            this.MaxValue.Margin = new System.Windows.Forms.Padding(2);
+            this.MaxValue.Name = "MaxValue";
+            this.MaxValue.Size = new System.Drawing.Size(76, 20);
+            this.MaxValue.TabIndex = 2;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(192, 11);
+            this.lblResult.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 13);
+            this.lblResult.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(11, 36);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(76, 19);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Рассчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(193, 70);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.MaxValue);
+            this.Controls.Add(this.lblMax);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMax;
+        private System.Windows.Forms.TextBox MaxValue;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ITMO.WinForms.ControlLab7_4/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITMO.WinForms.WinAsynchMethod7_3
+{
+    public partial class Form1 : Form
+    {
+        String str;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+                    }
+
+        private async Task<String> Eratosthenes(int max)
+        {
+            return await Task.Run(() =>
+            {
+                int maxValue = 0;
+                System.Text.StringBuilder resultText = new System.Text.StringBuilder();
+                if (int.TryParse(MaxValue.Text, out maxValue))
+                {
+                    for (int trial = 2; trial <= maxValue; trial++)
+                    {
+                        bool isPrime = true;
+                        for (int divisor = 2; divisor <= Math.Sqrt(trial); divisor++)
+                        {
+                            if (trial % divisor == 0) { isPrime = false; break; }
+                        }
+                        if (isPrime)
+                        {
+                            resultText.AppendFormat("{0} ", trial);
+                        }
+                    }
+                }
+                else
+                {
+                    resultText.Append("Unable to parse maximum value.");
+                }
+                resultText.ToString();
+                return resultText.ToString();
+
+            }
+            );
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            int a;
+            try
+            {
+                a = Int32.Parse(MaxValue.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("При выполнении преобразования типов возникла ошибка");
+                MaxValue.Text = "";
+                return;
+            }
+            string res = await Eratosthenes(a);
+            //lblResult.Text = res.ToString();
+            MessageBox.Show(res.ToString(), "Результат операции");
+        }
+
+
+    }
+}
